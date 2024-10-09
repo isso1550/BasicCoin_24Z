e60e93948f0dbc4572432f892f1b38471cc23b30
filiphorst <jafiliph@gmail.com>
2024-10-09 20:50:27 +0200

    poprawki callbackow, login

--- source/Wallet.js
+++ source/Wallet.js
@@ -9,28 +9,41 @@ dopasowałem się do małego narazie problemu. Jeśli masz sugestie poprawek to 
 
 
 
-const sqlite3 = require('sqlite3').verbose();
-const { generateKeyPair } = require('crypto');
+const Sqlite3 = require('sqlite3').verbose();
+const Crypto = require('crypto');
 
 /* DB SECTION */
-function connect_db (/*int*/port){
+function connect_db (/*int*/port, _callback){
     db_path = "./data/" + port + "/" + port + ".db"
     //TODO sprawdz czy jest odpowiedni podfolder w data i utworz jesli trzeba!
-    return db = new sqlite3.Database(db_path, sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE);
+    const db = new Sqlite3.Database(db_path, Sqlite3.OPEN_CREATE | Sqlite3.OPEN_READWRITE);
+    if (_callback){
+        _callback(db)
+    } else {
+        return db
+    }
 }
 
 function create_wallet (db, _callback) {
-    db.run(`CREATE TABLE if not exists Identities (name TEXT, id TEXT, pk TEXT, sk TEXT)`);
+    db.run(`CREATE TABLE if not exists Identities (name TEXT, id TEXT, pk TEXT, sk TEXT)`, () => {
+        if (_callback){
+            _callback()
+        }
+    });
 }
 function remove_wallet (db, _callback) {
-    db.run('DROP TABLE if exists Identities');
+    db.run('DROP TABLE if exists Identities', () => {
+        if (_callback){
+            _callback()
+        }
+    });
 }
 
 function save_identity(db, name, publicKey, privateKey, _callback) {
     db.get(`SELECT * From Identities WHERE name='${name}'`, (err, row) => {
         if (row==undefined) {
             db.run(`INSERT INTO Identities VALUES ('${name}', '${publicKey}', '${publicKey}', '${privateKey}')`)
-            console.log("Identity created")
+            console.log(`Identity ${name} created`)
             if (_callback){
                 _callback();
             }
@@ -47,23 +60,29 @@ function load_identity(db, name, _callback){
         if (row==undefined) {
             throw new Error("Identity not found")
         } else {
-            console.log(row['name'])
+            console.log(`Loading identity ${row['name']} ...`)
             if (_callback){
-                _callback(row['id'],row['sk'],row['pk'])
+                _callback(row['id'],row['pk'],row['sk'])
+            } else {
+                return [row['id'],row['pk'],row['sk']]
             }
         }
     })
 }
 
 function print_identities(db, _callback){
+    /*To debug*/
     db.all("SELECT * FROM Identities", (err,rows) => {
         console.log(rows)
+        if (_callback){
+            _callback()
+        }
     })
 }
 
 /* ASYM KEYS SECTION */
-function generate_identity(db, /*string*/name, /*string*/password, _callback) {
-    generateKeyPair('rsa', {
+function register(db, /*string*/name, /*string*/password, _callback) {
+    Crypto.generateKeyPair('rsa', {
         modulusLength: 4096,
         publicKeyEncoding: {
           type: 'spki',
@@ -80,20 +99,53 @@ function generate_identity(db, /*string*/name, /*string*/password, _callback) {
         save_identity(db, name, publicKey, privateKey, _callback)
       });   
 }
-function login(db, name, password){
-    throw new Error("Not implemented")
+function login(db, name, password, _callback){
+    load_identity(db, name, (id, pk, sk) => {
+        var publicKey = Crypto.createPublicKey({
+            'key': pk,
+            'format': 'pem',
+            'type': 'spki'
+        });
+        try{
+            var privateKey = Crypto.createPrivateKey({
+                'key': sk,
+                'format': 'pem',
+                'type': 'pkcs8',
+                'cipher': 'aes-256-cbc',
+                'passphrase': password
+            });
+        } catch (error){
+            console.log("Invalid credentials")
+            return
+        }  
+        console.log("LOGIN: Login succesful")
+        if (_callback){
+            _callback(id, publicKey, privateKey)
+        } else {
+            return [id, publicKey, privateKey]
+        }
+    })
+    
 }
 
 
-
+/* Testy troche na szybko */
 port = 5000
-db = connect_db(port)
-remove_wallet(db)
-create_wallet(db)
-generate_identity(db, "carfund","ilovecars", () => {
-    //print_identities(db)
-    load_identity(db, "carfund", (id, sk, pk) => {
-        console.log(sk)
+connect_db(port, (db) => {
+    remove_wallet(db, () => {
+        create_wallet(db, () => {
+            register(db, "carfund", "ilovecars", () => {
+                register(db, "main", "ilovemoney", () => {
+                    login(db, "carfund", "ilovecars", (id, pk, sk) => {
+                        console.log(pk, sk)
+                        //print_identities(db)
+                        login(db, "main", "ilovemoney", () => {
+                            console.log(pk, sk)                          
+                        })
+                    })
+                })
+            })
+        })
     })
-    
 })
+
